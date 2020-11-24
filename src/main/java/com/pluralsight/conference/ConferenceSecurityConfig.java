7b5121d79156f2eedc963d6c59fea595ab177c68
Zay-invalid <zayarhtet797@gmail.com>
2020-11-24 19:37:56 +0630

    day 2

--- src/main/java/com/pluralsight/conference/ConferenceSecurityConfig.java
+++ src/main/java/com/pluralsight/conference/ConferenceSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.pluralsight.conference;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -9,9 +10,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class ConferenceSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
@@ -31,8 +37,9 @@ public class ConferenceSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("bryan").password(passwordEncoder().encode("pass")).roles("USER");
+//        auth.inMemoryAuthentication()
+//                .withUser("bryan").password(passwordEncoder().encode("pass")).roles("USER");
+        auth.jdbcAuthentication().dataSource(dataSource);
     }
 
     @Bean
